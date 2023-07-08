b62b1e66360dda2250abb33cc13ae23d0ed89cd3
Bajcur <130090609+Bajcur@users.noreply.github.com>
2023-07-08 19:39:36 +0200

    Working mouseup

--- script.js
+++ script.js
@@ -7,11 +7,37 @@ function createGrid(){
         box.setAttribute("class", "box");
         container.appendChild(box);
         for(let i = 0; i < 16; i++){
+            const size = 430/16;
             const grid = document.createElement("div");
             grid.setAttribute("class", "grid");
+            grid.setAttribute("style", `width: ${size}px; height: ${size}px`);
             box.appendChild(grid);
         }
     }
 }
 
 createGrid();
+
+const grids = document.getElementsByClassName("grid");
+const container = document.getElementById("grid-container");
+let isDrawing = false;
+
+container.addEventListener("mousedown", () => {
+    isDrawing = true;
+    draw();      
+});
+window.addEventListener("mouseup", () => {
+    isDrawing = false;
+});
+    
+
+function draw(){
+    for (const grid of grids){
+        grid.addEventListener("mousemove", () => {
+            if (isDrawing){
+                grid.style.backgroundColor = "red";
+            }
+        })
+    }   
+}
+
