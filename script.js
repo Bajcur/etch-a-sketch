67b596ab1adf9f0ad2478e91e3601df46db0a1a1
Bajcur <130090609+Bajcur@users.noreply.github.com>
2023-07-08 20:42:47 +0200

    Add button creating the grid
    This commit adds button which asks user to input number of squares per side in the grid. Next step should be deleting existing one and then adding new after user pushes button again.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 'use strict';
 
-function createGrid(){
+function createGrid(n){
     const container = document.getElementById("grid-container");
-    for(let i = 0; i < 16; i++){
+    for(let i = 0; i < n; i++){
         const box = document.createElement("div");
         box.setAttribute("class", "box");
         container.appendChild(box);
-        for(let i = 0; i < 16; i++){
-            const size = 430/16;
+        for(let i = 0; i < n; i++){
+            const size = 430/n;
             const grid = document.createElement("div");
             grid.setAttribute("class", "grid");
             grid.setAttribute("style", `width: ${size}px; height: ${size}px`);
@@ -16,7 +16,19 @@ function createGrid(){
     }
 }
 
-createGrid();
+function userInput(){
+    let n = prompt("Enter a number")
+    if (n <= 64 ){
+        createGrid(n);
+    } else {
+        alert("Enter smaller number")
+        userInput();
+    }
+}
+
+const button = document.getElementById("size");
+button.addEventListener("click", () => userInput());
+//createGrid();
 
 const grids = document.getElementsByClassName("grid");
 const container = document.getElementById("grid-container");
