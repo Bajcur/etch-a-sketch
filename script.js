8d7d8ae1fbc01eb1c7ee6f24ff8fab7bb1c3c818
Bajcur <130090609+Bajcur@users.noreply.github.com>
2023-07-08 16:37:59 +0200

    Add the 16x16 grid
    This commit deletes placeholder divs from index.html. Adds function to script.js which is responsible for generating the 16x16 grid. Style.css is adjusted to show grid inside bordered box. Grid itself has no borders, so the box looks like it is empty but it contains 16x16 divs.

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 'use strict';
 
 function createGrid(){
-
+    const container = document.getElementById("grid-container");
+    for(let i = 0; i < 16; i++){
+        const box = document.createElement("div");
+        box.setAttribute("class", "box");
+        container.appendChild(box);
+        for(let i = 0; i < 16; i++){
+            const grid = document.createElement("div");
+            grid.setAttribute("class", "grid");
+            box.appendChild(grid);
+        }
+    }
 }
+
+createGrid();
